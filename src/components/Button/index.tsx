44cc5323fa8b76dc30e17a9ef6e7350734c18e13
Jhonata Sobral <jhonata.sobral@mobly.com.br>
2023-01-03 16:46:55 -0300

    config lint code

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -1,22 +1,17 @@
 import React from "react";
 import style from "./Button.module.scss";
 
-interface Props { 
-  children?: React.ReactNode, 
-  type?: "button" | "submit" | "reset" | undefined 
-  onClick?: () => void
+interface Props {
+  children?: React.ReactNode;
+  type?: "button" | "submit" | "reset" | undefined;
+  onClick?: () => void;
 }
 
-function Button({onClick, type, children}: Props) {
-
+function Button({ onClick, type, children }: Props) {
   return (
-    <button 
-      onClick={onClick}
-      type={type} 
-      className={style.button}
-    >
+    <button onClick={onClick} type={type} className={style.button}>
       {children}
     </button>
-  )
+  );
 }
 export default Button;
